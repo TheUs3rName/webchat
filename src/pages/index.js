932fc8552674ef2fae6cf82bfed5c9d39b67d5d6
TheUs3r <theus3rname@gmail.com>
2024-04-22 14:30:42 +0330

    added the help section.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import styles from "@/styles/Home.module.css";
+import { FcNext } from "react-icons/fc";
 
 export default function Home() {
-  return <div className={styles.main}></div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.section}>
+        <h5>select the chat with click on</h5>
+        <FcNext />
+      </div>
+    </div>
+  );
 }
