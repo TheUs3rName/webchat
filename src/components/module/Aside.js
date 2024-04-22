03799e510141497ca2059a8f5ab5aabebab7db9a
TheUs3r <theus3rname@gmail.com>
2024-04-22 17:20:45 +0330

    added the search logic

--- src/components/module/Aside.js
+++ src/components/module/Aside.js
@@ -2,11 +2,15 @@ import { getChatList } from "@/services/httpClient";
 import styles from "@/styles/Aside.module.css";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
+import { addManyChat } from "@/redux/chatSlice";
+import { useDispatch } from "react-redux";
 
 import { MdAccountCircle } from "react-icons/md";
 import { FcNext } from "react-icons/fc";
 
 import Loader from "./Loader";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 function Aside() {
   const { data, isLoading } = useQuery({
@@ -16,21 +20,49 @@ function Aside() {
     refetchOnWindowFocus: false,
   });
 
+  const [search, setSearch] = useState("");
+  const [filtered, setFiltered] = useState([]);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    !isLoading && dispatch(addManyChat(data));
+    if (search) {
+      const filteredChats = data.filter((chat) =>
+        chat.name.toLowerCase().includes(search.toLocaleLowerCase())
+      );
+      setFiltered(filteredChats);
+    } else {
+      setFiltered(data);
+    }
+  }, [isLoading, search]);
+
+  const router = useRouter();
+
+  const selectChatHandler = (_id) => {
+    router.push(`/chats/${_id}`);
+  };
+
   return (
     <div className={styles.aside}>
-      <input type="text" placeholder="serach" />
+      <input
+        type="text"
+        placeholder="serach"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       {isLoading ? (
         <Loader />
       ) : (
         <div className={styles.chat_list}>
           <ul>
-            {data.map((chat) => (
+            {filtered?.map((chat) => (
               <li className={styles.chat} key={chat._id}>
                 <span>
                   <div className={styles.blink}></div>
                   <h5>{chat.name}</h5>
                 </span>
-                <FcNext />
+                <FcNext onClick={() => selectChatHandler(chat._id)} />
               </li>
             ))}
           </ul>
